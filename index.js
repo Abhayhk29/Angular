26a5cd847402c1df665b3045ee693053a2ffc955
Abhay <abhayhk29@gmail.com>
2018-03-24 12:34:34 +0530

    ANgular Changes

--- index.js
+++ index.js
@@ -4,4 +4,26 @@
  * and open the template in the editor.
  */
 
+angular.module('invoice', [])
+        .controller('InvoiceController', function InvoiceController() {
+            this.qty = 1;
+            this.cost = 2;
+            this.inCurr = 'IN';
+            this.currencies = ['IN', 'PAK', 'R'];
+            this.usdToForeignRates = {
+                IN: 1,
+                PAK: 0.74,
+                R: 6.09
+            };
+            this.total = function total(outCurr) {
+                return this.convertCurrency(this.qty * this.cost, this.inCurr, outCurr);
+            };
+
+            this.convertCurrency = function convertCurrency(amount, inCurr, outCurr) {
+                return amount * this.usdToForeignRates[outCurr] / this.usdToForeignRates[inCurr];
+            };
+            this.pay = function pay() {
+                window.alert('Thanks!');
+            };
+        });
 
